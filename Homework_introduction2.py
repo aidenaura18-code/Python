ebf214a7c3f79b232e3d0238929d507662001a26
aidenaura18-code <aidenaura67@gmail.com>
2026-05-20 17:26:29 +0400

    Add files via upload

---
+++ Homework_introduction2.py
@@ -0,0 +1,2 @@
+print("congratulations my friend for getting a gold medal in the olympics in the states of games")
+print("CONGRATULATIONS MY FRIEND FOR GETTING A GOLD MEDAL IN THE OLYMPICS IN THE STATES OF GAMES")

c16be013b585cdabf4d10bb08b95bf64f6a06753
aidenaura18-code <aidenaura67@gmail.com>
2026-05-20 18:08:24 +0400

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,10 @@
+#avrange
+tree1= 10
+tree2= 20
+tree3= 45
+tree4= 123
+tree5=96
+total= tree1+tree2+tree3+tree4+tree5
+print("the sum of all the trees is", total)
+average= total/5
+print("the average of all the trees is", average)

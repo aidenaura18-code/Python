a3b93a3ed4d3d286d252c62010bad30333a10a1d
aidenaura18-code <aidenaura67@gmail.com>
2026-06-17 18:12:36 +0400

    Add files via upload

---
+++ Exprestion.py
@@ -0,0 +1,63 @@
+num= 10+5*2
+print(num)
+
+
+num=10+5*2-4
+print(num)
+
+
+num=(10+5)*2-4
+print(num)
+
+
+num = 20-6/2
+print(num)
+
+
+num=2**4+2
+print(num)
+
+Book=50
+pen=10
+sum=(Book*3)+(pen*4)
+print(sum)
+
+
+pizza=200
+sol=pizza*4
+print(sol)
+
+
+shirt=1000
+sol=shirt-250
+print(sol)
+
+sol=7*4
+print(sol)
+
+
+sol+=12*8-6
+print(sol)
+
+sol=25-10/2+7
+print(sol)
+
+v=4
+w=5
+x=8
+y=2
+
+z=(v+w)*x/y;
+print(z)
+
+name = "Alexa"
+age= 0
+
+if name == "Alexa" or name == "John":
+
+    print("Hello, welcome!")
+else:
+    print("goodbye!")
+
+add=30-12/3+9
+print(add)

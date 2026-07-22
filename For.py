ab68fe04d87ef3032b0c80c5fc4c30c8e0fd4fef
aidenaura18-code <aidenaura67@gmail.com>
2026-07-22 15:12:02 +0400

    Add files via upload

---
+++ For.py
@@ -0,0 +1,27 @@
+#for loop
+#A for loop is used when we know how many times we want to repeat something.
+
+
+for i in range (5):
+    print("hello")
+
+for i in range(2,5):
+    print("hello")
+
+for i in range(5):
+    print(i)
+
+n="hello"
+for i in n:
+    print(i)
+
+for i in range(11):
+    print(i)
+
+mul=5
+for i in range(1,11):
+    print(mul,"*",i,"=",mul*i)
+
+mul=2
+for i in range(1,11):
+    print(mul,"x",i,"=",mul*i)

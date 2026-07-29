1e55533ce1cf199531b0a4926f924efc884215dc
aidenaura18-code <aidenaura67@gmail.com>
2026-07-29 15:11:47 +0400

    Add files via upload

---
+++ while.py
@@ -0,0 +1,44 @@
+num= 1  # while loop : a while loop is used when we don't know how many times we want to repeat something.
+
+while num <= 5:
+    print(num)
+    num+= 1
+
+
+
+
+num=1
+while num <=  3:
+    print(" hello")
+    num+=1
+
+
+
+x=1
+
+while x <= 5:
+    print(x)
+    x += 1
+
+
+
+password= " "
+
+while password !="python 123":
+    password =input("enter the password: ")
+
+print("access granted")
+
+
+bottels= 1
+
+while bottels <= 3:
+    print("filled bottles",bottels)
+    bottels+=1
+
+
+
+num=1
+while num <= 5:
+    print('Aiden')
+    num+=1

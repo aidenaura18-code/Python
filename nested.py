201e373eb447944e5cde4685a247f028715bf770
aidenaura18-code <aidenaura67@gmail.com>
2026-07-30 16:07:04 +0400

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,37 @@
+for i in range (1,4):
+   for  j in range (1,3):
+      print("i=",i,"j=",j)
+
+
+
+
+
+
+i=1
+while i <= 3:
+   j=1
+   while j <= 2:
+      print("i=",i,"j=",j)
+      j+= 1
+
+   i +=1
+
+
+
+
+
+
+for student in range (1,4):
+    for subject in range (1,3):
+        print("student=",student,"subject=",subject)
+
+
+
+i = 1
+while i <= 3:
+
+   j = 1
+   while j <= 2:
+      print(i,j)
+      j=+ 1
+   i += 1

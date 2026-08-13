6c6a3e2e5893b7ec2f41b5beed993e716a560728
aidenaura18-code <aidenaura67@gmail.com>
2026-08-13 16:08:13 +0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,29 @@
+def greet():
+    print("Hello, World!")
+
+greet()
+greet()
+
+
+def welcome():
+    print("hello welcome")
+
+
+welcome()
+
+
+def Person (name, age):
+    print("hello",name,"your age is ",age)
+
+
+Person("john",20)
+Person("neha",25)
+Person('joe',30)
+
+
+
+def add(a,b):
+    return a+b
+
+result=add(2,5)
+print(result)
